32a1686ce204e77c6f476754660867eed20a1771
Matt Jacobs <mnj1222@gmail.com>
2018-05-22 09:19:27 -0500

    Move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
 
-console.log("Loaded react-dom");
-
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Cloudwalker Digital",
+    'image': {
+      'desc': "Cloudwalker Digital Wordpress Site",
+      'src': "images/Cloudwalker(1024x1024).png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Serverless Portfolio Site",
+    'image': {
+      'desc': "Serverless Portfolio Site",
+      'src': "images/example2.png",
+      'comment': `"Chemistry” by Surian Soosay is licensed under CC BY 2.0
+           https://www.flickr.com/photos/ssoosay/4097410999"`
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181"`
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
